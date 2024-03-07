91ddecc0b533de04f3398278e028fe59c6f4e890
jozefntes <123389422+jozefntes@users.noreply.github.com>
2024-03-06 21:52:51 -0600

    Remove base path from astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jozefntes.github.io',
-  base: '/my-repo',
   integrations: [tailwind()]
 });
