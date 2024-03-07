8217192dc8e08534d1cbe9183ea2a437ea513acc
jozefntes <123389422+jozefntes@users.noreply.github.com>
2024-03-06 21:46:36 -0600

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astronaut.github.io',
+  site: 'https://jozefntes.github.io',
   base: '/my-repo',
   integrations: [tailwind()]
 });
